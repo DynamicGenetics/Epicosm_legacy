3033ccba4eb76c6acd3f8c9cbc5f63548905a4db
Al Tanner <8190834+altanner@users.noreply.github.com>
2019-03-23 18:01:35 +0000

    Update timer.sh

--- timer.sh
+++ timer.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
+
 # periodically runs twongo, and updates current status
 # the time duration between runs can be modified by altering
 # the sleep time (in seconds), on line 19.
 
 touch /root/host_interface/STATUS;
+sed -i '1,2d' /root/host_interface/STATUS;
+echo -e '\n' > /root/host_interface/STATUS;
 
 while true;
     
-    LATEST_HARVEST=`ls -ls /root/host_interface/output/csv | tail -1 | awk '{print $NF}' | sed 's/.csv//'`;
     TWONGO_STATUS="Twongo is currently harvesting.";
     sed -i "1s/.*/$TWONGO_STATUS/" /root/host_interface/STATUS;
-    sed -i "2s/.*/The most recent harvest was $TWONGO_STATUS/" /root/host_interface/STATUS;
-    do /usr/bin/python3 /twongo_files/twongo.py --refresh --log;
+    do /usr/bin/python3 /twongo/twongo.py --refresh;
     TWONGO_STATUS="Twongo is currently idle.";
+    LATEST_HARVEST=`ls -ls /root/host_interface/output/csv | tail -1 | awk '{print $NF}' | sed 's/.csv//'`;
     sed -i "1s/.*/$TWONGO_STATUS/" /root/host_interface/STATUS;
-    sed -i "2s/.*/The most recent harvest was $TWONGO_STATUS/" /root/host_interface/STATUS;
+    sed -i "2s/.*/The most recent harvest was $LATEST_HARVEST/" /root/host_interface/STATUS;
 
-    sleep 3600;
+    sleep 3200;
     
     done;
+    

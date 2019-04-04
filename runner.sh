095c6da3d4c36d998cc2a38063e2f64ffcd7492f
Al Tanner <8190834+altanner@users.noreply.github.com>
2019-04-04 15:22:56 +0100

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Docker starter
 
-echo "[o_o] twongo is about to start _.~\"~._.~\"~._.~\"~._.~\"~.__.~\"~._.~\"~";
+echo "[o_o] twongo container runner _.~\"~._.~\"~._.~\"~._.~\"~.__.~\"~._.~\"~";
 echo "Please have your credentials file and user_list in this run folder."
 
 read -p "How often would you like to harvest (in hours)? " frequency;
@@ -32,11 +32,16 @@ fi
 frequency_in_seconds=$(($frequency*3600))
 
 echo "OK, twongo starting, harvesting once every $frequency hours.";
-echo "To stop this process, run this command: docker stop \$(docker ps -a -q) "
-echo "(This will stop all running containers, to stop this one specifically, please search for the container id.)"
-
-echo $frequency_in_seconds
-echo $refresh
-echo $log
+secs=5
+while [ $secs -gt 0 ]; do
+   echo -ne "Starting in $secs\033[0K\r"
+   sleep 1
+   : $((secs--))
+done
 
 docker run -d -v $PWD:/root/host_interface/ altanner/twongo:latest /bin/bash -c "while true; do /usr/bin/python3 /twongo/twongo.py $refresh $log; sleep $frequency_in_seconds; done"
+
+sleep 3;
+container_name=`docker ps | sed -n 2p | awk 'END {print $NF}'`
+echo "Your container has been assigned the name $container_name"
+echo "To end this process, run this command: docker stop $container_name"

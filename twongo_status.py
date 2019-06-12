ad68a19e01ee375c7c65cd5b785791932a52c69b
Al Tanner <8190834+altanner@users.noreply.github.com>
2019-06-12 18:39:27 +0100

    Update twongo_status.py

--- twongo_status.py
+++ twongo_status.py
@@ -3,12 +3,18 @@ import os
 
 
 def status_up(collection, status_file):
+    
+    """ Update STATUS file in run folder to notify when running """
+    
     tweet_count = collection.count_documents({})
     with open(status_file, 'w+') as status:
         status.write(f"Twongo is currently running.\nThis process started at {datetime.datetime.now().strftime('%H:%M:%S_%d-%m-%Y')}\nThe database currently contains {tweet_count} tweets.\n")
 
 
 def status_down(collection, status_file, run_folder):
+                     
+    """ Update STATUS file in run folder to notify when idle, and next run """
+                     
     tweet_count = collection.count_documents({})
     with open(status_file, 'w+') as status:
         if os.path.isfile(run_folder + '.frequency'):

a466d2211ddf3e4a6aaad4032d0d3be50369fdfb
Al Tanner <8190834+altanner@users.noreply.github.com>
2019-06-19 12:27:09 +0100

    More refactoring and tidying... more to go tho

--- twitter_ops.py
+++ twitter_ops.py
@@ -3,12 +3,14 @@ import os
 import sys
 import pymongo
 import tweepy
-from mongo_ops import index_mongo
 
-def lookup_users(run_folder, screen_names, api, duplicate_users, not_found):
+
+def lookup_users(run_folder, screen_names, api):
 
     """convert twitter screen names into persistent id numbers"""
 
+    duplicate_users = []
+    not_found = []
     if '--refresh' not in sys.argv and os.path.exists(run_folder + "user_list.ids"):
         return
     with open(run_folder + "user_list") as file:
@@ -51,7 +53,7 @@ def lookup_users(run_folder, screen_names, api, duplicate_users, not_found):
             id_file.write("%s\n" % id)                            # write to id file
    
 
-def get_tweets(twitter_id, db, api, collection, empty_users, private_users):
+def get_tweets(run_folder, twitter_id, db, api, collection, empty_users, private_users):
 
     """acquire tweets from each user id number and store them in MongoDB"""
 
@@ -59,7 +61,7 @@ def get_tweets(twitter_id, db, api, collection, empty_users, private_users):
     if db.tweets.count_documents({"user.id": twitter_id}) > 0:
         ## we already have this user's timeline, just get recent tweets
         try:
-            print(f"User {twitter_id} is in the database, shallow acquisition cycle...")
+            print(f"User {twitter_id} is in the database, normal acquisition cycle...")
             alltweets = []
             new_tweets = api.user_timeline(id=twitter_id, count=200,
                                            tweet_mode='extended', exclude_replies=True)
@@ -93,16 +95,21 @@ def get_tweets(twitter_id, db, api, collection, empty_users, private_users):
 
     ## update the database with the acquired tweets for this user
     for tweet in alltweets:
-        try:
+    #    tweet_id = tweet._json['id'] # redundant for now
+    #    if tweet._json['id'] > max_tweet_id:
             try:
-                collection.update_one(tweet._json, {'$set': tweet._json}, upsert=True)
-            except pymongo.errors.DuplicateKeyError:
-                pass
-        except IndexError:
-            print(f"User {twitter_id} has no tweets to insert.")
+                try:
+                    collection.insert_one(tweet._json, {'$set': tweet._json})
+                except pymongo.errors.DuplicateKeyError:
+                    pass # denies duplicates being added
+            except IndexError:
+                print(f"User {twitter_id} has no tweets to insert.")
 
 
-def get_friends(twitter_id): ## get the "following" list for this user
+def get_friends(twitter_id):
+
+    """Get the following list for this user"""
+
     friend_list = []
     try:
         for friend in tweepy.Cursor(api.friends_ids, id = twitter_id, count = 200).pages():
@@ -116,36 +123,43 @@ def get_friends(twitter_id): ## get the "following" list for this user
         print(f"Problem putting friends into MongoDB...")
 
 
-def harvest(run_folder, db, api, collection, empty_users, private_users, not_found, duplicate_users):
-    index_counter = 0
+def harvest(run_folder, db, api, collection):
+
+    """Get tweet timelines and insert new tweets into MongoDB"""
+
+    empty_users = []
+    private_users = []
+    not_found = []
+    duplicate_users = []
     ## generate user id list from user2id output file
     users_to_follow = [int(line.rstrip('\n')) for line in open(run_folder + "user_list.ids")]
     now = datetime.datetime.now()
     print(f"\nStarting tweet harvest at {now.strftime('%H:%M:%S_%d-%m-%Y')} ...")
     try: ## iterate through this list of ids.
         for twitter_id in users_to_follow:
-            if index_counter % 100 == 0: # every 100 users index the database
-                index_mongo(run_folder, db)
-            get_tweets(twitter_id, db, api, collection, empty_users, private_users)  ## get all their tweets and put into mongodb
+            get_tweets(run_folder, twitter_id, db, api, collection, empty_users, private_users)  ## get all their tweets and put into mongodb
+
             if '--getfriends' in sys.argv:
                 get_friends(twitter_id) ## this tends to rate limit, but tweet harvest doesn't (?!)
-            index_counter += 1
-        print()
+
         if len(duplicate_users) > 0: # if users are not found, put into missing user file
             print(f"Info: {len(set(duplicate_users))} user names are duplicates (see user_list.duplicates)")
             with open(run_folder + "user_list.duplicates", 'w') as duplicate_user_file:
                 for duplicate_user in set(duplicate_users):
                     duplicate_user_file.write("%s\n" % duplicate_user)    # write to missing user file
+
         if len(not_found) > 0: # if users are not found, put into missing user file
             print(f"Info: {len(not_found)} user names do not have accounts (see user_list.notfound)")
             with open(run_folder + "user_list.notfound", 'w') as missing_user_file:
                 for missing_user in not_found:
                     missing_user_file.write("%s\n" % missing_user)    # write to missing user file
+
         if len(empty_users) > 0: # if users are empty, put into empty users file
             print(f"Info: {len(empty_users)} users have empty accounts (see user_list.empty)")
             with open(run_folder + "user_list.empty", 'w') as empty_user_file:
                 for empty_user in empty_users:
                     empty_user_file.write("%s\n" % empty_user)    # write to empty user file  
+
         if len(private_users) > 0: # if users are private found, put into private user file
             print(f"Info: {len(private_users)} users have private accounts (see user_list.private)")
             with open(run_folder + "user_list.private", 'w') as private_user_file:

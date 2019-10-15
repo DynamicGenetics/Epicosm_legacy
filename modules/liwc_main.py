ef410503219b780875509b1d84f349264dcfc920
Al Tanner <8190834+altanner@users.noreply.github.com>
2019-10-15 15:04:28 +0100

    Update liwc_main.py

--- modules/liwc_main.py
+++ modules/liwc_main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from pandas import DataFrame
 import sys
+import os
 import liwc
 import re
 from collections import Counter
@@ -10,8 +11,10 @@ from datetime import datetime
 # Set up the dataframe by importing tweet.csv and setting up columns
 def set_up_dataframe(csv_file, category_names):
     '''build a dataframe with a tweet for each row, append word count and categories'''
-    df = DataFrame(pd.read_csv(csv_file))
-    df['created_at'] = df['created_at'].apply(lambda d: datetime.strptime(d, '%a %b %d %H:%M:%S %z %Y').strftime('%Y-%m-%d'))
+    # the read_csv engine must be python, not default of c, or some lines will mess with it
+    df = DataFrame(pd.read_csv(csv_file, encoding='utf-8', engine='python', error_bad_lines=False))
+    df['created_at'] = df['created_at'].apply(lambda d: datetime.strptime(d, '%a %b %d %H:%M:%S %z %Y').strftime('%d-%m-%Y'))
+    df['word_count'] = 0     # addend wc (consistent with official liwc format output)
     for category in category_names:
         df[category] = 0.0   # append the category columns
     return df
@@ -38,18 +41,26 @@ def count_and_insert(df, parse_fn):
 
 if __name__ == '__main__':
 
+    # check that inputs exist.
     if len(sys.argv) != 3:
         print(f'Please assign your dictionary and file to analyse.')
         print(f'eg: python3 main.py LIWC.dic some.txt')
         exit(0)
+    if not os.path.isfile(sys.argv[1]):
+        print(f"The dictionary", sys.argv[1], "doesn't seem to exist here.")
+        exit(0)
+    if not os.path.isfile(sys.argv[2]):
+        print(f"The CSV file", sys.argv[2], "doesn't seem to exist here.")
+        exit(0)
+
 
     # bring the dictionary in, sys.argv[1] is the dictionary name
     parse, category_names = liwc.load_token_parser(sys.argv[1])
     df = set_up_dataframe(csv_file=sys.argv[2], category_names=category_names)
     count_and_insert(df, parse_fn=parse)
-    df_anonymised = df.drop(['user.id_str', 'full_text'], axis=1)
+    df_anonymised = df.drop(['id_str', 'full_text'], axis=1)
     df_anonymised.to_csv('LIWC_' + sys.argv[2], sep=',', encoding='utf-8')
-   # print(df_anonymised)
+    #print(df_anonymised)
 
 
 

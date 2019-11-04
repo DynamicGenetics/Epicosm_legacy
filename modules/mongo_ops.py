8906a5d4b989e04571cfb9344c7e5879a73f8c02
Al Tanner <8190834+altanner@users.noreply.github.com>
2019-11-04 15:14:08 +0000

    Update mongo_ops.py

--- modules/mongo_ops.py
+++ modules/mongo_ops.py
@@ -5,28 +5,6 @@ import time
 import psutil
 import pymongo
 
-## why does this complain that variables are not defined?
-## I am defining them!
-def check_mongo():
-
-    """check if MongoDB is present and correct"""
-
-    try:
-        mongod_executable_path = subprocess.check_output(['which', 'mongod']).decode('utf-8').strip()
-    except:
-        print(f"You don't seem to have MongoDB installed. Stopping.")
-        sys.exit()
-    try:
-        mongoexport_executable_path = subprocess.check_output(['which', 'mongoexport']).decode('utf-8').strip()
-    except:
-        print(f"Mongoexport seems missing... stopping.")
-        sys.exit()
-    try:
-        mongodump_executable_path = subprocess.check_output(['which', 'mongodump']).decode('utf-8').strip()
-    except:
-        print(f"Mongodump seems missing... stopping.")
-        sys.exit()
-    return mongod_executable_path, mongoexport_executable_path, mongodump_executable_path
 
 def start_mongo(mongod_executable_path, db_path, db_log_filename):
 

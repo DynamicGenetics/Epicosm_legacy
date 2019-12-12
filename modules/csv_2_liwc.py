41bf7c917cb2dc6e3fca775984b2d312b856e590
Al Tanner <8190834+altanner@users.noreply.github.com>
2019-12-12 08:48:25 +0000

    Update csv_2_liwc.py

--- modules/csv_2_liwc.py
+++ modules/csv_2_liwc.py
@@ -10,14 +10,14 @@ from datetime import datetime
 
 # Set up the dataframe by importing tweet.csv and setting up columns
 def set_up_dataframe(csv_file, category_names):
-    
+
     """ Build a dataframe with a tweet for each row.
-    
+
     Rows are tweets, columns are time tweeted to the day,
     and to keep consistency with LIWC official output
     we add append word count and categories (otherwise if it appends
     them later it can get messy"""
-    
+
     # the read_csv engine must be python, not default of c, or some lines will mess with it
     df = DataFrame(pd.read_csv(csv_file, encoding='utf-8', engine='python', error_bad_lines=False))
     df['created_at'] = df['created_at'].apply(lambda d: datetime.strptime(d, '%a %b %d %H:%M:%S %z %Y').strftime('%d-%m-%Y'))
@@ -41,16 +41,18 @@ def load_dictionary(dictionary):
 
 
 def count_and_insert(df, parse_fn):
-    
+
     """Assign each word to dictionary category and put in dataframe"""
-    
+
     index = 0
-#    df['word_count'] = df['full_text'].apply(lambda x: len(str(x).split(' ')))
     df['word_count'] = df['text'].apply(lambda x: len(str(x).split(' ')))
-    #for tweet in df['full_text']:
     for tweet in df['text']:
         text_tokens = tokenize(tweet)
-        text_counts = Counter(category for token in text_tokens for category in parse_fn(token))
+        try:
+            text_counts = Counter(category for token in text_tokens for category in parse_fn(token))
+        except Exception: # if the text is "1" it throws a TypeError. are bots doing this? tweets are not visible if you look them up.
+            print("Exception happened, text was", tweet)
+            pass
         for count_category in text_counts: # insert the LIWC values as proportion of word_count
             df.at[index, count_category] = text_counts[count_category] / (df.iloc[index]['word_count'])
         index += 1
@@ -81,12 +83,12 @@ def csv_2_liwc_run():
     parse, category_names = liwc.load_token_parser(sys.argv[1])
     df = set_up_dataframe(csv_file=sys.argv[2], category_names=category_names)
     count_and_insert(df, parse_fn=parse)
-#    df_anonymised = df.drop(['id_str', 'full_text', 'retweeted_status.full_text'], axis=1)
     df_anonymised = df.drop(['id_str', 'text', 'retweeted_status.full_text'], axis=1)
     output_filename = os.path.splitext(sys.argv[2])[0] + '_LIWC.csv'
     df_anonymised.to_csv(output_filename, sep=',', encoding='utf-8')
 
 
 if __name__ == '__main__':
-    
+
     csv_2_liwc_run()
+    

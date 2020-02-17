37a9e5eea28b23f5aad37bf3a3075edacba4ffff
altanner <8190834+altanner@users.noreply.github.com>
2020-02-17 13:55:33 +0000

    fix status update

--- epicosm.py
+++ epicosm.py
@@ -15,13 +15,17 @@ from modules import mongo_ops, epicosm_meta, twitter_ops, env_config
 start = time.time()
 now = datetime.datetime.now()
 
+# Set paths as instance of EnvironmentConfig
+env = env_config.EnvironmentConfig()
 
 def signal_handler(sig, frame):
+
     """Handle interrupt signals, eg ctrl-c (and other kill signals).
     
     Exiting more abruptly can leave MongoDB running, which can cause issues,
     so Mongo is asked to stop
     """
+    status_file = env.status_file
     print(f"\n\nJust a second while I try to exit gracefully...")
     with open(status_file, 'w+') as status:
         status.write(f"Epicosm is currently idle, but was interruped by user on last run.\nThe most recent harvest\
@@ -51,9 +55,6 @@ except:
     print(f"Mongodump seems missing... stopping.")
     sys.exit()
 
-# Set paths as instance of EnvironmentConfig
-env = env_config.EnvironmentConfig()
-
 # Check user list exists and get it
 if not os.path.exists(env.run_folder + '/user_list'):
     print(f"USAGE: please provide a list of users to follow, named 'user_list'.")
@@ -78,42 +79,44 @@ if not os.path.exists(env.run_folder + '/epicosm_logs'):
 ## run it ##
 ############
 
+def main():
+
+    # set up logging
+    epicosm_meta.logger_setup(env.epicosm_log_filename)
+    # bring the credentials in from credentials.txt
+    credentials = twitter_ops.get_credentials()
+    # start mongodb
+    mongo_ops.start_mongo(mongod_executable_path,
+                          env.db_path,
+                          env.db_log_filename)
+    # modify status file
+    epicosm_meta.status_up(env.status_file)
+    # tidy up the database for better efficiency
+    mongo_ops.index_mongo(env.run_folder)
+    # get persistent user ids from screen names
+    twitter_ops.lookup_users(env.run_folder, screen_names, credentials)
+    # get tweets for each user and archive in mongodb
+    twitter_ops.harvest(env.run_folder, credentials)
+    # if user wants the friend list, make it
+    # !!! this is very slow, so is an option
+    if '--get_following' in sys.argv:
+        twitter_ops.get_following(env.run_folder, credentials)
+    # create CSV file
+    mongo_ops.export_csv(mongoexport_executable_path, env.csv_filename)
+    # create JSON file
+    # !!! this is big, slow and fields cannot be specified, so an option
+    if '--json' in sys.argv:
+        mongo_ops.export_json(mongoexport_executable_path, env.json_filename)
+    # backup database into BSON
+    mongo_ops.backup_db(mongodump_executable_path, env.database_dump_path)
+    # modify status file
+    epicosm_meta.status_down(env.status_file, env.run_folder)
+    # shut down mongodb
+    mongo_ops.stop_mongo()
+
+    print(f"\nAll done, Epicosm finished at {datetime.datetime.now().strftime('%H:%M:%S_%d-%m-%Y')}, taking around {int(round((time.time() - start) / 60))} minutes.")
+
+
 if __name__ == '__main__':
+    main()
 
-  #  try:
-        # set up logging
-        epicosm_meta.logger_setup(env.epicosm_log_filename)
-        # bring the credentials in from credentials.txt
-        credentials = twitter_ops.get_credentials()
-        # start mongodb
-        mongo_ops.start_mongo(mongod_executable_path,
-                              env.db_path,
-                              env.db_log_filename)
-        # modify status file
-        epicosm_meta.status_up(env.status_file)
-        # tidy up the database for better efficiency
-        mongo_ops.index_mongo(env.run_folder)
-        # get persistent user ids from screen names
-        twitter_ops.lookup_users(env.run_folder, screen_names, credentials)
-        # get tweets for each user and archive in mongodb
-        twitter_ops.harvest(env.run_folder, credentials)
-        # if user wants the friend list, make it
-        # !!! this is very slow, so is an option
-        if '--get_following' in sys.argv:
-            twitter_ops.get_following(env.run_folder, credentials)
-        # create CSV file
-        mongo_ops.export_csv(mongoexport_executable_path, env.csv_filename)
-        # create JSON file
-        # !!! this is big, slow and fields cannot be specified, so an option
-        if '--json' in sys.argv:
-            mongo_ops.export_json(mongoexport_executable_path, env.json_filename)
-        # backup database into BSON
-        mongo_ops.backup_db(mongodump_executable_path, env.database_dump_path)
-        # modify status file
-        epicosm_meta.status_down(env.status_file, env.run_folder)
-        # shut down mongodb
-        mongo_ops.stop_mongo()
-
-        print(f"\nAll done, Epicosm finished at {datetime.datetime.now().strftime('%H:%M:%S_%d-%m-%Y')}, taking around {int(round((time.time() - start) / 60))} minutes.")
-#    except:
- #       pass

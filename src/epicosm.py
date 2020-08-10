a96818771bc67f80d12bc6e2865a0a4c04068707
Al Tanner <al.tanner@gmail.com>
2020-08-10 10:24:55 +0100

    schedulre frequency

--- src/epicosm.py
+++ src/epicosm.py
@@ -14,7 +14,7 @@ from modules import mongo_ops, epicosm_meta, twitter_ops, env_config, mongodb_co
 valid_args = ["--user_harvest", "--id_harvest", "--get_following",
               "--repeat", "--refresh"]
 
-usage = ["Epicosm: usage (full details: dynamicgenetics.github.io/Epicosm/)\n\n" + 
+usage = ["Epicosm usage (full details: dynamicgenetics.github.io/Epicosm/)\n\n" + 
          "Please provide flags:\n\n" +
          "--user_harvest        Harvest tweets from all users from a file called user_list\n" +
          "                      (provided by you) with a single user per line.\n\n" + 
@@ -37,18 +37,50 @@ usage = ["Epicosm: usage (full details: dynamicgenetics.github.io/Epicosm/)\n\n"
          "nohup ./epicosm --user_harvest --refresh --repeat &\n"]
 
 
+# Check the time
+start = time.time()
+now = datetime.datetime.now()
+
+# Set paths as instance of EnvironmentConfig
+env = env_config.EnvironmentConfig()
+
+
+# Catch ctrl-c signals (and kill -15 signals)
+def signal_handler(sig, frame):
+
+    """Handle interrupt signals, eg ctrl-c (and other kill signals).
+    
+    Exiting more abruptly can leave MongoDB running, which can cause issues,
+    so Mongo is asked to stop
+    """
+    status_file = env.status_file
+    print(f"Just a second while I try to exit gracefully...")
+    mongo_ops.stop_mongo(env.db_path)
+    sys.exit()
+
+
+# Check if this is being run as native or compiled python
+def native_or_compiled():
+
+    run_method = "native python"
+
+    if getattr(sys, "frozen", False):
+        # we are running in a bundle
+        run_method = "compiled python"
+        bundle_dir = sys._MEIPASS
+
+    else:
+        # we are running native python
+        bundle_dir = os.path.dirname(os.path.abspath(__file__))
+
+    print("Epicosm launching as", run_method)
+
+
 ############
 ## run it ##
 ############
 
 def main():
-
-    # Check the time
-    start = time.time()
-    now = datetime.datetime.now()
-
-    # Set paths as instance of EnvironmentConfig
-    env = env_config.EnvironmentConfig()
 
     # print help message if no/wrong args provided
     if len(sys.argv) < 2 or not all(arg in valid_args for arg in sys.argv[1:]):
@@ -56,79 +88,62 @@ def main():
         sys.exit(0)
 
     # check running method
-    epicosm_meta.native_or_compiled()
-
+    native_or_compiled()
     # check environment
-    (mongod_executable_path, mongoexport_executable_path,
-    mongodump_executable_path, screen_names) = epicosm_meta.check_env()
-
+    mongod_executable_path, mongoexport_executable_path, mongodump_executable_path, screen_names = epicosm_meta.check_env()
+    epicosm_meta.check_env()
     # set up logging
     epicosm_meta.logger_setup(env.epicosm_log_filename)
-
     # setup signal handler
-    signal.signal(signal.SIGINT, epicosm_meta.signal_handler)
-
+    signal.signal(signal.SIGINT, signal_handler)
     # verify credentials
     credentials, auth, api = twitter_ops.get_credentials() #this doesn't do anything.
-
     # start mongodb
     mongo_ops.start_mongo(mongod_executable_path,
                           env.db_path,
                           env.db_log_filename,
                           env.epicosm_log_filename)
-
     # modify status file
     epicosm_meta.status_up(env.status_file)
-
     # tidy up the database for better efficiency
     mongo_ops.index_mongo(env.run_folder)
-
+    print(*credentials)
     # get persistent user ids from screen names
     twitter_ops.lookup_users(env.run_folder, screen_names, credentials, auth, api)
-
     # get tweets for each user and archive in mongodb
     if "--user_harvest" in sys.argv:
         twitter_ops.harvest(env.run_folder, credentials, auth, api,
-                            mongodb_config.client, mongodb_config.db, mongodb_config.collection)
-
+                            mongodb_config.client, mongodb_config.db, mongodb_config.collection) #why does this work if no credentials?!
     # if user wants the friend list, make it
     if "--get_following" in sys.argv:
-        twitter_ops.get_following(env.run_folder, credentials, auth,
-                                  api, mongodb_config.following_collection)
+        twitter_ops.get_following(env.run_folder, credentials, auth, api, mongodb_config.following_collection)
         sys.argv.remove("--get_following") # we only want to do this once
-
     # create CSV file
     mongo_ops.export_csv(mongoexport_executable_path,
                          env.csv_filename,
                          env.epicosm_log_filename)
-
     # create JSON file
     if "--json" in sys.argv:
         mongo_ops.export_json(mongoexport_executable_path,
                               env.json_filename,
                               env.epicosm_log_filename)
-
     # backup database into BSON
     mongo_ops.backup_db(mongodump_executable_path,
                         env.database_dump_path,
                         env.epicosm_log_filename)
-
     # modify status file
     epicosm_meta.status_down(env.status_file, env.run_folder)
-
     # shut down mongodb
     mongo_ops.stop_mongo(env.db_path)
 
-    print(f"Scheduled task finished at {datetime.datetime.now().strftime('%Y-%m-%d_%H:%M:%S')}.\n")
-    print(f"Epicosm has been up for about {int(round((time.time() - start)) / 86400 )} days.")
+    print(f"Scheduled task finished at {datetime.datetime.now().strftime('%Y-%m-%d_%H:%M:%S')}. Epicosm has been up for about {int(round((time.time() - start)) / 86400 )} days.")
 
 
 if __name__ == "__main__":
 
     if ("--repeat" in sys.argv):
         main()
-#        schedule.every(3).days.at("06:00").do(main)
-        schedule.every(30).seconds.do(main)
+        schedule.every(3).days.at("06:00").do(main)
         while True:
             schedule.run_pending()
             time.sleep(15)
